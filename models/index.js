6896a83e42655bbf761b624ddedaeacaa368f432
Bret Banger <Banger484@gmail.com>
2022-04-28 19:51:17 -0500

    fixed an error in my model index js

--- models/index.js
+++ models/index.js
@@ -6,19 +6,21 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 })
 // Categories have many Products
 Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_id'
+  through: 'ProductTag',
+  foreignKey: 'product_id',
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'tag_id'
+  through: 'ProductTag',
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
